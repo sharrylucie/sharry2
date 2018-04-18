6e4cea81e763d0fe1186e2a17a1069bac32233ce
lucie moreau <luciem.moreau@gmail.com>
2018-04-18 17:21:29 +0200

    normal navbar + header

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/design'
-
-  get 'pages/about'
-
-  get 'pages/living'
-
-  get 'pages/home'
-
-  get 'pages/investor'
-
-  get 'pages/contact'
+  get 'design', to: 'pages#design', as: :design
+  get 'about', to: 'pages#about', as: :about
+  get 'home', to: 'pages#home', as: :home
+  get 'living', to: 'pages#living', as: :living
+  get 'investor', to: 'pages#investor', as: :investor
+  get 'contact', to: 'pages#contact', as: :contact
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
