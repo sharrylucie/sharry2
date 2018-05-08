e6bece189144985ce4d6f8cb8ebf52f663f97f2c
lucie moreau <luciem.moreau@gmail.com>
2018-05-08 15:32:01 +0200

    mailchimp email ajusted

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,12 +1,16 @@
 // app/javascript/components/navbar.js
+
 function initUpdateNavbarOnScroll() {
-  const navbar = document.querySelector('.navbar-wagon');
-  window.addEventListener('scroll', () => {
-    if (window.scrollY >= window.innerHeight) {
-      navbar.classList.remove('navbar-home');
-    } else {
-      navbar.classList.add('navbar-home');
-    }
+  document.addEventListener("DOMContentLoaded", function(event) {
+    const navbar = document.querySelector('.navbar-wagon');
+    window.addEventListener('scroll', () => {
+      console.log('');
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.remove('navbar-home');
+      } else {
+        navbar.classList.add('navbar-home');
+      }
+    });
   });
 }
 
