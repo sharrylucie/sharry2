317e60d54a841f3ed5d6629b3b34c935253b1b86
lucie moreau <luciem.moreau@gmail.com>
2018-05-14 16:28:13 +0200

    banner width 1200px

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,7 +1,6 @@
 // app/javascript/components/navbar.js
 
 function initUpdateNavbarOnScroll() {
-  document.addEventListener("DOMContentLoaded", function(event) {
     const navbar = document.querySelector('.navbar-wagon');
     window.addEventListener('scroll', () => {
       console.log('');
@@ -11,7 +10,7 @@ function initUpdateNavbarOnScroll() {
         navbar.classList.add('navbar-home');
       }
     });
-  });
+  );
 }
 
 export { initUpdateNavbarOnScroll };
